592e888a4c0551187cd35673af72e0f2f816a328
armen_chester <armen.khachatryan@click2sure.co.za>
2019-03-29 22:38:55 +0400

    Implement simple spell checker

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spell Checker
+
+Install package 
+
+    $ pip install -i https://testpypi.python.org/pypi check_spell
+
+Usage:
+
+    import check_spell
+    
+    check_spell.check(text for checking)
+    
+Returns list of words which are wrongly spelled.
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='check_spell',
+    version='0.1',
+    description='Spell checker',
+    url='https://github.com/armenchester/check_spell',
+    author='Armen Khachatryan',
+    author_email='armen.khachatryan88@gmail.com',
+    license='MIT',
+    packages=['check_spell'],
+    include_package_data=True
+)
